2fdb23304c5bf0f334d08616291c4d4cb62628a4
Martin Brook <Martin@ceres.(none)>
2013-01-19 17:30:45 +0000

    Created barebones project setup

---
+++ src/com/martinbrook/ghostsvsheroes/GhostsVsHeroes.java
@@ -0,0 +1,27 @@
+package com.martinbrook.ghostsvsheroes;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GhostsVsHeroes extends JavaPlugin {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return super.onCommand(sender, command, label, args);
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+
+}
